7d078f4a273e82233b6bf0f677dd914a8aa54e86
abobsawwdstydf <200753405+abobsawwdstydf@users.noreply.github.com>
2025-03-22 13:55:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+    devtools: {enabled: false},
+    modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+    css: ['~/assets/css/style.css'],
+    devServer: {
+        port: 3001,
+    },
+    routeRules: {
+        '/': {
+            prerender: true,
+        }
+    },
+    app: {
+        head: {
+            title: 'CF AI Web',
+            meta: [
+                {
+                    name: 'keywords',
+                    content: 'CF AI Web, AI, Cloudflare Workers, ChatGPT, GeminiPro, Google Generative AI'
+                },
+                {
+                    name: 'description',
+                    content: 'Integrated web platform supporting Gemini Pro/Cloudflare Workers AI/ChatGPT by Jazee6'
+                }
+            ],
+            link: [
+                {
+                    rel: 'manifest',
+                    href: '/manifest.json'
+                }
+            ],
+            noscript: [
+                {
+                    innerHTML: 'This website requires JavaScript.'
+                }
+            ]
+        }
+    },
+    i18n: {
+        vueI18n: './i18n.config.ts',
+        strategy: 'no_prefix',
+        defaultLocale: 'zh',
+    }
+    // nitro: {
+    //     vercel: {
+    //         regions: ["sin1", "syd1", "sfo1", "iad1", "pdx1", "cle1"]
+    //     }
+    // }
+})
